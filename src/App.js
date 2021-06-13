7888ba9ec8087b5ac823361017e1bda942679989
Bell Oluwatomisin <bello4aus@gmail.com>
2021-06-13 20:11:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      todos: [],
+      modal: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false,
+      },
+    };
+  }
+
+ 
+
+  componentDidMount(){
+    this.getTodos();
+  }
+
+
+  getTodos(){
+    axios
+    .get("/api")
+    .then(res => {
+      this.setState({todos:res.data});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        {this.state.todos.map(item => (
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <p>{item.body}</p>
+          <p>Hello</p>
+        </div>))}
+        </div>
+
+    );}
+};
+
+export default App;
